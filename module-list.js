a738b37b15dbfbf6ff68c409061d76a6f9601959
Gunnar Unger <gunnar.unger@gmail.com>
2021-11-14 09:33:05 +1100

    update

---
+++ module-list.js
@@ -0,0 +1,20 @@
+(function(){
+    var modules={
+        "not_elig":         {url:"$H/m/not_elig.html"},
+        "eligible":    {url:"$H/m/eligible.html",router:1},
+        "survey-data": {url:"$H/m/elig-questions-data.html",Table:"asteriod-survey-record",form_module:"survey-form",router:1},
+        "survey-form": {url:"$H/m/elig-questions-form.html",Table:"asteriod-survey-record"},
+        "dsa-survey-data": {url:"$H/m/dsa-data.html",Table:"asteriod-dsa-survey-record",form_module:"dsa-survey-form",router:1},
+        "dsa-survey-form": {url:"$H/m/dsa-form.html",Table:"asteriod-dsa-survey-record",router:1},
+        
+    }
+    for(p in modules){
+        $vm.module_list[p]=modules[p];
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    if(window.location.toString().indexOf('tb=demo')!=-1){
+        for(p in $vm.module_list){
+            $vm.module_list[p].Table="demo-"+$vm.module_list[p].Table;
+        }
+    }
+})();
